fff055c0f248f929a477d4de6db3058884e1dc08
unknown <klencodes@gmail.com>
2021-03-03 04:16:06 +0000

    configured app for hosting

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
+// const express = require('express');
+// const app = express();
+// app.use(express.static(__dirname + '/dist'));
+// app.listen(process.env.PORT || 8080);
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
 // app.use(forceSSL());
 const path = require('path');
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// //Install express server
-// const express = require('express');
-// const path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// // Serve only the static files form the dist directory
-// app.use(express.static('./dist/angular-app-heroku'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/frontend'));
 
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-// );
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/frontend/'}),
+);
 
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
