ab5ada7e650583d81e077e5aa1d1de09b60d579f
unknown <klencodes@gmail.com>
2021-03-03 04:48:04 +0000

    Update @angular cli

--- server.js
+++ server.js
@@ -1,35 +1,8 @@
-// const express = require('express');
-// const app = express();
-// app.use(express.static(__dirname + '/dist'));
-// app.listen(process.env.PORT || 8080);
-// const forceSSL = function() {
-//   return function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       return res.redirect(
-//        ['https://', req.get('Host'), req.url].join('')
-//       );
-//     }
-//     next();
-//   }
-// }
-// app.use(forceSSL());
-const path = require('path');
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/loan-ui'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/loan-ui/'}),
-);
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist/loan-ui'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/loan-ui/index.html'));});
 app.listen(process.env.PORT || 8080);
