cf4589e2cf2e1f52f37d9b8ff7bfa89c10cc9073
unknown <klencodes@gmail.com>
2021-03-03 04:20:58 +0000

    configured app for hosting

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/frontend'));
+app.use(express.static('./dist/loan-ui'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/frontend/'}),
+    res.sendFile('index.html', {root: 'dist/loan-ui/'}),
 );
 
 // Start the app by listening on the default Heroku port
