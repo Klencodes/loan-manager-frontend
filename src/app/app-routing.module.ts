769bc3c80958715eeb161f73265c6fe55c1f5505
unknown <klencodes@gmail.com>
2021-03-10 01:42:08 +0000

    modifications

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,11 @@ const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard],
       children: [
         {path: '', component: OverviewComponent },
-        { path: 'loans', loadChildren: () => import('./components/layout/home/loans/loans.module').then(m => m.LoansModule) },
-        { path: 'payments', loadChildren: () => import('./components/layout/home/payments/payments.module').then(m => m.PaymentsModule) },
-        { path: 'invoices', loadChildren: () => import('./components/layout/home/invoices/invoices.module').then(m => m.InvoicesModule) },
-        { path: 'reports', loadChildren: () => import('./components/layout/home/reports/reports.module').then(m => m.ReportsModule) },
-        { path: 'settings', loadChildren: () => import('./components/layout/home/settings/settings.module').then(m => m.SettingsModule) },        
+        { path: 'loans', loadChildren: () => import('./components/layout/home/_overview/loans/loans.module').then(m => m.LoansModule) },
+        { path: 'payments', loadChildren: () => import('./components/layout/home/_overview/payments/payments.module').then(m => m.PaymentsModule) },
+        { path: 'invoices', loadChildren: () => import('./components/layout/home/_overview/invoices/invoices.module').then(m => m.InvoicesModule) },
+        { path: 'reports', loadChildren: () => import('./components/layout/home/_overview/reports/reports.module').then(m => m.ReportsModule) },
+        { path: 'settings', loadChildren: () => import('./components/layout/home/_overview/settings/settings.module').then(m => m.SettingsModule) },        
       ] 
     },
     
